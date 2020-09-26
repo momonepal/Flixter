02a317852775f781f2db878611e666207a056129
mohit <mohitkc44@gmail.com>
2020-09-25 21:10:52 -0400

    Initial Commit

---
+++ flixterTests/flixterTests.swift
@@ -0,0 +1,34 @@
+//
+//  flixterTests.swift
+//  flixterTests
+//
+//  Created by Mohit on 9/25/20.
+//  Copyright © 2020 nmn. All rights reserved.
+//
+
+import XCTest
+@testable import flixter
+
+class flixterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
